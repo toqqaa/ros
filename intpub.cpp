537d7cb00f5b1cf7d3d0bf9ffd5722f75db5250a
toqqaa <145803764+toqqaa@users.noreply.github.com>
2023-11-19 22:16:23 +0200

    Update intpub.cpp

--- intpub.cpp
+++ intpub.cpp
@@ -1,40 +1,40 @@
 #include "ros/ros.h"
-#include "std_msgs/Int16.h"
+#include "std_msgs/Int32.h"
+
+
+
 
 
 int main(int argc, char **argv)
 {
- 
+  
   ros::init(argc, argv, "int_pub_node");
 
-
+  
   ros::NodeHandle n;
 
-  ros::Publisher int_pub = n.advertise<std_msgs::Int16>("integer", 1000);
+  
+  ros::Publisher int_pub = n.advertise<std_msgs::Int32>("integer", 1000);
 
   ros::Rate loop_rate(10);
-   for (int i = 20; i <= 100; i++)
-    {
-        std::cout<<"int number =" <<i<< std::endl;
-        
-    }
-
-    std_msgs::Int16 msg;
-    msg.data ;
-   
-    ROS_INFO("%d", msg.data);
-    int_pub.publish(msg); 
 
 
-    
-  int count = 0;
-  while (ros::ok())
+  int count = 20;
+  while (ros::ok()&& count<=100)
   {
+    
+    std_msgs::Int32 int_msg;
+   int_msg.data = count;
+
+    ROS_INFO("%i", int_msg.data);
+
+
+   int_pub.publish(int_msg);
 
     ros::spinOnce();
 
     loop_rate.sleep();
-   
+    ++count;
   }
 
 
